f7f64511f496ed212741019f3d0c54c2f4b97b9a
irene <irene.schimperna@gmail.com>
2020-05-04 21:16:21 +0200

    Aggiunto plot dei dati per settimana

--- plot_dati.m
+++ plot_dati.m
@@ -7,5 +7,51 @@ dati = table2array(caricoDEday);
 figure(1)
 plot(dati(:,3),'o-')
 grid on
+title('Plot brutale dei dati')
 
-% Preriodicità annuale, preriodicità settimanale e trend in salita
+x_vec=(1:size(dati))';
+
+dati = [dati x_vec];
+
+dati_dom=[];
+dati_lun=[];
+dati_mar=[];
+dati_mer=[];
+dati_gio=[];
+dati_ven=[];
+dati_sab=[];
+
+for i=1:size(dati)
+   switch(dati(i,2))
+       case 1
+           dati_dom=[dati_dom; dati(i,:)];
+       case 2
+           dati_lun=[dati_lun; dati(i,:)];
+       case 3
+           dati_mar=[dati_mar; dati(i,:)];
+       case 4
+           dati_mer=[dati_mer; dati(i,:)];
+       case 5
+           dati_gio=[dati_gio; dati(i,:)];
+       case 6
+           dati_ven=[dati_ven; dati(i,:)];
+       case 7
+           dati_sab=[dati_sab; dati(i,:)];
+   end
+end
+
+figure(2)
+plot(dati_lun(:,4),dati_lun(:,3),'o-')
+grid on
+hold on
+plot(dati_mar(:,4),dati_mar(:,3),'o-')
+plot(dati_mer(:,4),dati_mer(:,3),'o-')
+plot(dati_gio(:,4),dati_gio(:,3),'o-')
+plot(dati_ven(:,4),dati_ven(:,3),'o-')
+plot(dati_sab(:,4),dati_sab(:,3),'o-')
+plot(dati_dom(:,4),dati_dom(:,3),'o-')
+
+legend('Lunedi', 'Martedi', 'Mercoledi','Giovedi','Venerdi','Sabato','Domenica')
+title('Dati collegati in base al giorno della settimana')
+% Se si vogliono vedere i dati relativi a un unico giorno della settimana
+% basta commentare le linee di codice dei plot che non si vogliono vedere
