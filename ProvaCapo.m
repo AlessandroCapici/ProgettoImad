e9676438add677e280baa9d8fbb3624b08776d1a
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-05-05 18:35:57 +0200

    cose a caso

--- ProvaCapo.m
+++ ProvaCapo.m
@@ -1,19 +1,32 @@
 %% per non fare casino
 clear;
+clc;
 close all;
 %% caricamento dati
 load('caricoDEday');
 load = table2array(caricoDEday);
 %% variabili a caso
-dayOfYear = load(:,1);
-dayOfWeek = load(:,2);
+giorni_anno = load(:,1);
 carico = load(:,3);
 x= (1:length(carico))';
 carico_n = normalize(carico);
-n_dati = length(carico);
+n = length(carico);
 
 %%  plot dei dati
 figure(1);
-scatter(x,carico,'v');
-plot(load);
+plot(x,carico_n,'o-');
+grid on;
+hold on;
 
+%% ls model 
+q=5;
+Phi=ones(n,q);
+thetaLS=Phi\carico_n;
+carico_hat=Phi*thetaLS;
+epsilon= carico-carico_hat;                 %MI DA TUTTO NaN MADONNA MAIALA.......non so il perchè
+SSR=epsilon'*epsilon;
+var_hat= SSR/(n-q);
+var_thetaLS=var_hat*(inv(Phi'*Phi));
+std_thetaLS=sqrt(var_thetaLS);
+plot(x,carico_hat);
+legend('dati','stima');
