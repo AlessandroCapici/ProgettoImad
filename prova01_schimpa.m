a1eb4ccfcba31591c6418e1faabcd0930454924c
irene <irene.schimperna@gmail.com>
2020-05-06 21:59:00 +0200

    Aggiunta eliminazione delle righe NaN

--- prova01_schimpa.m
+++ prova01_schimpa.m
@@ -8,41 +8,52 @@ close all
 load('caricoDEday.mat')
 dati = table2array(caricoDEday);
 
+x_vec = (1:size(dati,1))';
+dati =  [dati x_vec];
+%% Eliminazione dei NaN
+emptyRows=[];
+for i=dati(:,4)'
+    if isnan(dati(i,3))
+        emptyRows=[emptyRows i];
+    end
+end
+
+emptyRows=flip(emptyRows);  % Rimuovo i NaN a partire dalla fine se no mi scalano gli indici e rimuovo righe non NaN
+for i=emptyRows
+    disp(i)
+    dati(i,:)=[]; 
+end
 %% Variabili utili
 dayOfYear = dati(:,1);
 dayOfWeek = dati(:,2);
 carico = dati(:,3);
-x_vec = (1:length(carico))';
 carico_n = normalize(carico);
 n_dati = length(carico);
 
 %% Identificazione del modello
 %Per identificare il modello uso solo una parte dei dati (non so perchè se uso più dati thetaLS mi esce NaN)
-inizio_dati=100;
-dati_usati=300;
 
-Phi = [ones(dati_usati+1,1) ...x_vec(inizio_dati:dati_usati+inizio_dati)...
-    cos(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi/365) sin(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi/365)...
-    ...cos(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi*2/365) sin(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi*2/365)...
-    cos(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi/7) sin(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi/7)...
-    cos(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi*2/7) sin(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi*2/7)...
-    cos(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi*3/7) sin(x_vec(inizio_dati:dati_usati+inizio_dati)*2*pi*3/7)];
-
-[thetaLS,var_theta,SSR] = stimaLS(carico_n(inizio_dati:dati_usati+inizio_dati),Phi);
-disp(thetaLS)
+Phi = [ones(728,1) dati(1:end,4)...
+    cos(dati(1:end,4)*2*pi/365) sin(dati(1:end,4)*2*pi/365)...
+    cos(dati(1:end,4)*2*pi/7) sin(dati(1:end,4)*2*pi/7)...
+    cos(dati(1:end,4)*2*pi*2/7) sin(dati(1:end,4)*2*pi*2/7)...
+    cos(dati(1:end,4)*2*pi*3/7) sin(dati(1:end,4)*2*pi*3/7)];
+thetaLS=Phi\carico
+% [thetaLS,var_theta,SSR] = stimaLS(carico_n,Phi);
+% disp(thetaLS)
 
 %% Plot di modello e dati
-Phi1 = [ones(n_dati,1) ...x_vec 
-    cos(x_vec*2*pi/365) sin(x_vec*2*pi/365)...
+Phi1 = [ones(size(dati,1),1) dati(:,4)...
+    cos(dati(:,4)*2*pi/365) sin(dati(:,4)*2*pi/365)...
     ...cos(x_vec*2*pi*2/365) sin(x_vec*2*pi*2/365)...
-    cos(x_vec*2*pi/7) sin(x_vec*2*pi/7)...
-    cos(x_vec*2*pi*2/7) sin(x_vec*2*pi*2/7)...
-    cos(x_vec*2*pi*3/7) sin(x_vec*2*pi*3/7)];
+    cos(dati(:,4)*2*pi/7) sin(dati(:,4)*2*pi/7)...
+    cos(dati(:,4)*2*pi*2/7) sin(dati(:,4)*2*pi*2/7)...
+    cos(dati(:,4)*2*pi*3/7) sin(dati(:,4)*2*pi*3/7)];
 
 figure(1)
-plot(x_vec,carico_n,'o-')
+plot(dati(:,4),carico,'o-')
 grid on
 hold on
-plot(x_vec,Phi1*thetaLS)
+plot(dati(:,4),Phi1*thetaLS)
 
 legend('dati', 'stima')
