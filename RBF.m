8632c5c8d483d55bb855a0da2411fc17ecf0b4fc
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-05-08 15:04:40 +0200

    Rbf che in realta e una rete a caso

--- RBF.m
+++ RBF.m
@@ -1,1 +1,63 @@
-%% usare scaricare toolbox
+%% per non fare casino
+clear;
+clc;
+close all;
+%% caricamento dati
+load('caricoDEday');
+load = table2array(caricoDEday);
+
+x_vec = (1:size(load,1))';
+load =  [load x_vec];
+%% Eliminazione dei NaN
+emptyRows=[];
+for i=load(:,4)'
+    if isnan(load(i,3))
+        emptyRows=[emptyRows i];
+    end
+end
+emptyRows=flip(emptyRows);  
+for i=emptyRows
+    load(i,:)=[]; 
+end
+%% variabili a caso
+giorni_anno = load(:,1);
+carico = load(1:365,3);
+x= (1:length(carico));
+carico_n = normalize(carico)';
+n = length(carico);
+
+%% Creo la rete neurale
+nnetwork=fitnet([10,8,5]);
+...Size of the hidden layers in the network, specified as a row vector. 
+...The length of the vector determines the number of hidden layers in the network.
+...Example: For example, you can specify a network with 3 hidden layers, 
+...where the first hidden layer size is 10, the second is 8, and the third is 5 as follows: [10,8,5]
+...The input and output sizes are set to zero. The software adjusts the sizes of these during training according to the training data.
+
+%% Setto alcuni parametri
+nnetwork.trainParam.show=50;
+nnetwork.trainParam.lr=0.05;    %li ho copiati da internet ahahha
+nnetwork.trainParam.epochs=300;
+
+%% Fase di learning
+nnetwork=train(nnetwork,x,carico_n); 
+simulazione=sim(nnetwork,x);
+figure(1);
+plot(simulazione);
+grid on;
+hold on;
+plot(carico_n);
+hold on;
+legend('simulazione','dati');
+%% Fase di test
+carico_test=load(365:end,3);
+carico_test=normalize(carico_test)';
+x_test=(1:length(carico_test));
+simtest=sim(nnetwork,x_test);
+figure(2);
+grid on;
+hold on;
+plot(simtest);
+hold on;
+plot(carico_test);
+legend('simulazione','dati');
