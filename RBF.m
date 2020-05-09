2601bbb736072210e319703657c2914ec7dc984e
irene <irene.schimperna@gmail.com>
2020-05-09 10:50:46 +0200

    Aggiunto plot dei centroidi in RBF

--- RBF.m
+++ RBF.m
@@ -26,9 +26,22 @@ giorni_settimana = load(1:end,2);
 
 %% cluser dei dati
 [idx,ctrs,sumd]= kmeans(x,4);
+figure(1)
 gscatter(giorni_settimana,x,idx);
-legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Centroids');
-hold off;
+hold on
+X = [0.7 7.3];
+plot(X,[ctrs(1) ctrs(1)])
+plot(X,[ctrs(2) ctrs(2)])
+plot(X,[ctrs(3) ctrs(3)])
+plot(X,[ctrs(4) ctrs(4)])
+legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Centroid 1','Centroid 2',...
+    'Centroid 3', 'Centroid 4');
+hold off
+title('Dati divisi per cluster e giorno della settimana')
 
-
+figure(2)
+gscatter(load(:,4),x,idx)
+legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4');
+grid on
+title('Dati plottati nel tempo e divisi per cluster')
 
