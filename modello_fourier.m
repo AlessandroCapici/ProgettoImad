8ac7be9df8468f4e7d4035ebfd7273954d078833
irene <irene.schimperna@gmail.com>
2020-05-11 13:12:31 +0200

    Prove delle due Ire

---
+++ modello_fourier.m
@@ -0,0 +1,80 @@
+clear
+close all
+clc
+
+%% Caricamento dati
+load('caricoDEday.mat')
+dati = table2array(caricoDEday);
+
+x_vec = (1:size(dati,1))';
+dati =  [dati x_vec];
+%% Eliminazione dei NaN
+emptyRows=[];
+for i=dati(:,4)'
+    if isnan(dati(i,3))
+        emptyRows=[emptyRows i];
+    end
+end
+
+% Stimo i dati mancanti
+for i = emptyRows
+    dati(i,3)= (dati(i-7,3)+dati(i+7,3))/2;
+end
+
+mean_year1 = mean(dati(1:365,3));
+mean_year2 = mean(dati(366:end,3));
+
+vec=[mean_year1*ones(365,1); mean_year2*ones(365,1)];
+
+dati(:,3) = dati(:,3)-vec;
+
+%% Pulizia vacanze natalizie
+
+rowsToDelete = [];
+for i=dati(:,4)'
+   if dati(i,1)<7 | dati(i,1)>356
+       rowsToDelete = [rowsToDelete i];
+   end
+end
+
+rowsToDelete = flip(rowsToDelete);
+
+for j = rowsToDelete
+    dati(j,:)=[];
+end
+
+%%
+n = 365;
+
+Phi = [ones(n,1) ...
+    cos(dati(1:n,1)*2*pi/365) sin(dati(1:n,1)*2*pi/365)...
+    cos(dati(1:n,1)*2*pi*2/365) sin(dati(1:n,1)*2*pi*2/365)...
+    cos(dati(1:n,1)*2*pi*3/365) sin(dati(1:n,1)*2*pi*3/365)...
+   ... cos(dati(1:n,1)*2*pi*4/365) sin(dati(1:n,1)*2*pi*4/365)...
+    cos(dati(1:n,2)*2*pi/7) sin(dati(1:n,2)*2*pi/7)...
+    cos(dati(1:n,2)*2*pi*2/7) sin(dati(1:n,2)*2*pi*2/7)...
+    cos(dati(1:n,2)*2*pi*3/7) sin(dati(1:n,2)*2*pi*3/7)...
+    cos(dati(1:n,2)*2*pi*4/7) sin(dati(1:n,2)*2*pi*4/7)];
+
+[thetaLS,var_theta,SSR] = stimaLS(dati(1:n,3),Phi);
+
+n=size(dati,1);
+Phi_p =  [ones(n,1) ...
+    cos(dati(1:n,1)*2*pi/365) sin(dati(1:n,1)*2*pi/365)...
+    cos(dati(1:n,1)*2*pi*2/365) sin(dati(1:n,1)*2*pi*2/365)...
+    cos(dati(1:n,1)*2*pi*3/365) sin(dati(1:n,1)*2*pi*3/365)...
+  ...  cos(dati(1:n,1)*2*pi*4/365) sin(dati(1:n,1)*2*pi*4/365)...
+    cos(dati(1:n,2)*2*pi/7) sin(dati(1:n,2)*2*pi/7)...
+    cos(dati(1:n,2)*2*pi*2/7) sin(dati(1:n,2)*2*pi*2/7)...
+    cos(dati(1:n,2)*2*pi*3/7) sin(dati(1:n,2)*2*pi*3/7)...
+    cos(dati(1:n,2)*2*pi*4/7) sin(dati(1:n,2)*2*pi*4/7)];
+
+Y_hat = Phi_p * thetaLS;
+
+figure(1)
+
+plot(dati(:,4),dati(:,3))
+hold on
+plot(dati(:,4),Y_hat)
+grid on
+legend('Dati','Stima')
---
+++ pulizia_del_trend.m
@@ -0,0 +1,80 @@
+clear
+close all
+clc
+
+%% Caricamento dati
+load('caricoDEday.mat')
+dati = table2array(caricoDEday);
+
+x_vec = (1:size(dati,1))';
+dati =  [dati x_vec];
+%% Eliminazione dei NaN
+emptyRows=[];
+for i=dati(:,4)'
+    if isnan(dati(i,3))
+        emptyRows=[emptyRows i];
+    end
+end
+
+% Stimo i dati mancanti
+for i = emptyRows
+    dati(i,3)= (dati(i-7,3)+dati(i+7,3))/2;
+end
+
+%% Stima del trend lineare
+
+Phi = [ones(size(dati,1),1) dati(:,4)];
+thetaLS = Phi\dati(:,3);
+
+trend = Phi*thetaLS;
+figure(1)
+plot(dati(:,3),'o-')
+hold on
+plot(trend)
+grid on
+
+figure(2)
+plot(dati(:,3)-trend)
+grid on
+
+
+%% Trend come media annuale
+
+
+mean_year1 = mean(dati(1:365,3));
+mean_year2 = mean(dati(366:end,3));
+
+vec=[mean_year1*ones(365,1); mean_year2*ones(365,1)];
+
+dati1=dati;
+dati1(:,3) = dati1(:,3)-vec;
+
+figure(3)
+plot(dati1(:,3))
+grid on
+
+% Questa è la versione che ci piace di più
+
+%% Trend dalla media mobile
+
+A=1;
+B=ones(1,7)/7;
+y_mediamobile=filter(B,A,dati(:,3));
+
+sum=0;
+for i=1:7
+   sum = sum + dati(i,3);
+   y_mediamobile(i)=sum/i;
+end
+
+thetaLS_mm = Phi\y_mediamobile;
+trend1 = Phi*thetaLS_mm;
+figure(4)
+plot(y_mediamobile,'o-')
+hold on
+plot(trend1)
+grid on
+
+figure(5)
+plot(dati(:,3)-trend1)
+grid on
