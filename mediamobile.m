5816d211752b5fe2d0b48c2faf633f6b0c86f4b7
irene <irene.schimperna@gmail.com>
2020-05-16 16:10:51 +0200

    Prove del modello annuale

--- mediamobile.m
+++ mediamobile.m
@@ -21,6 +21,8 @@ for i = emptyRows
     dati(i,3)= (dati(i-7,3)+dati(i+7,3))/2;
 end
 
+
+%% Media mobile
 A=1;
 B=ones(1,7)/7;
 y_mediamobile=filter(B,A,dati(:,3));
@@ -29,6 +31,24 @@ sum=0;
 for i=1:7
    sum = sum + dati(i,3);
    y_mediamobile(i)=sum/i;
+end
+
+mm = [dati(:,1) dati(:,4) y_mediamobile];
+
+
+%% Pulizia vacanze natalizie
+
+rowsToDelete = [];
+for i=mm(:,2)'
+   if mm(i,1)<7 | mm(i,1)>356
+       rowsToDelete = [rowsToDelete i];
+   end
+end
+
+rowsToDelete = flip(rowsToDelete);
+
+for j = rowsToDelete
+    mm(j,:)=[];
 end
 
 %% Plot di tutti i dati
@@ -40,9 +60,9 @@ title('Plot brutale dei dati')
 %% Plot della media mobile
 
 figure(2)
-plot(y_mediamobile)
+plot(mm(:,2),mm(:,3))
 grid on
-title('Plot della media mobile')
+title('Plot della media mobile senza Natale')
 
 %% Plot dati a cui ho tolto la media mobile
 
